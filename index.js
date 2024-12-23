b755a6ef771d3c925e2cdf8548cbf9986ee0bdd9
NureAlam68 <nurealam151068@gmail.com>
2024-12-24 00:16:34 +0600

    get room by id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 const app = express();
@@ -26,16 +26,30 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
 
+    const database = client.db('luxewayDb');
+    const roomsCollection = database.collection('rooms');
 
-    
+    app.get('/rooms', async(req, res) => {
+      const result = await roomsCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/room/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = { _id : new ObjectId(id) };
+      const result = await roomsCollection.findOne(query);
+      res.send(result);
+    })
+       
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
